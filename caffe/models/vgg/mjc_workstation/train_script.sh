6262234280a8b3aa23f5f298915b3a59db0e8beb
JiachenMao <jim35@pitt.edu>
2016-10-26 13:12:44 -0400

    add vgg script

---
+++ caffe/models/vgg/mjc_workstation/train_script.sh
@@ -0,0 +1,4 @@
+./../../../build/tools/caffe train --solver=./vgg16_solver_group_lasso.prototxt --gpu=1 
+#--weights=./models/vgg16.caffemodel --gpu=1 
+#--weights=./models/vgg16_original.caffemodel
+#--snapshot_prefix ./models/vgg16_original
