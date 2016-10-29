56224c1d221c91975f5c28dcfe8b0afebf6ee79e
JiachenMao <jim35@super-titan.ee.pitt.edu>
2016-10-29 15:04:54 -0400

    now able to fine-tuning vgg

---
+++ caffe/.build_release/src/caffe/util/mmio.d
@@ -0,0 +1,4 @@
+.build_release/src/caffe/util/mmio.o: src/caffe/util/mmio.cpp \
+ include/caffe/util/mmio.hpp
+
+include/caffe/util/mmio.hpp:
--- caffe/models/vgg/mjc_workstation/train_script.sh
+++ caffe/models/vgg/mjc_workstation/train_script.sh
@@ -1,4 +1,5 @@
-./../../../build/tools/caffe train --solver=./vgg16_solver_group_lasso.prototxt --gpu=1 
-#--weights=./models/vgg16.caffemodel --gpu=1 
+#./../../../build/tools/caffe train --solver=./solver.prototxt --weights=./models/vgg16.caffemodel --gpu 1
+./../../../build/tools/caffe train --solver=./vgg16_solver_group_lasso.prototxt --weights=./models/snapshot_iter_1668.caffemodel --gpu=1 
 #--weights=./models/vgg16_original.caffemodel
 #--snapshot_prefix ./models/vgg16_original
+#./../../../build/tools/caffe train --solver=./vgg16_solver_group_lasso.prototxt --snapshot ./models/snapshot_iter_1668.solverstate
