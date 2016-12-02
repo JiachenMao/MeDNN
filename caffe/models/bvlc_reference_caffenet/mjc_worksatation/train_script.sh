5233cd7eae1b7f883b4aa06e0dc6fb9cfa39072d
JiachenMao <jim35@super-titan.ee.pitt.edu>
2016-12-02 11:32:38 -0500

    add new version of caffe with SDNN

--- caffe/models/bvlc_reference_caffenet/mjc_worksatation/train_script.sh
+++ caffe/models/bvlc_reference_caffenet/mjc_worksatation/train_script.sh
@@ -3,7 +3,7 @@
 #--weights=./models/vgg16_original.caffemodel
 #--snapshot_prefix ./models/vgg16_original
 #./../../../build/tools/caffe train --solver=./vgg16_solver_group_lasso.prototxt --snapshot ./models/snapshot_iter_1668.solverstate
-./../../../build/tools/caffe train --solver=./solver_lasso.prototxt --weights=./models/caffenet_SSL_0.4259.caffemodel --gpu=1
+./../../../build/tools/caffe train --solver=./solver_lasso.prototxt --weights=./models/caffenet_SSL_0.4469.caffemodel --gpu=1
 
 
 
